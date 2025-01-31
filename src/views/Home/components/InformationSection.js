051f9d315d41f2ce5592db6eb4f0b5f501aa0db1
fredrick <m.mwangi.fredrick@gmail.com>
2025-01-31 12:34:20 +0300

    Remove founders section

--- src/views/Home/components/InformationSection.js
+++ src/views/Home/components/InformationSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import About from './About';
-import FoundersList from './FoundersList';
+// import FoundersList from './FoundersList';
 
 const InformationSection = () => {
   return (
@@ -9,9 +9,9 @@ const InformationSection = () => {
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <About />
       </Grid>
-      <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-        <FoundersList />
-      </Grid>
+      {/* <Grid item xs={12} sm={12} md={6} lg={6} xl={6}> */}
+      {/*  <FoundersList /> */}
+      {/* </Grid> */}
     </Grid>
   );
 };
